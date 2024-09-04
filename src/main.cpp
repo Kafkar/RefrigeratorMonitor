a143303afbc53d4a49f8e11f5ddaafe636a4968f
Kafkar <Niels@kafkar.nl>
2024-09-04 20:21:23 +0200

    read 2 temps to display

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,99 @@
-#include <Arduino.h>
+//Scanning (SDA : SCL) - GPIO4 : GPIO5 - I2C device found at address 0x3C  !
+//Scanning (SDA : SCL) - D2 : D1 - I2C device found at address 0x3C  !
 
-// put function declarations here:
-int myFunction(int, int);
 
+/**************************************************************************
+ This is an example for our Monochrome OLEDs based on SSD1306 drivers
+
+ Pick one up today in the adafruit shop!
+ ------> http://www.adafruit.com/category/63_98
+
+ This example is for a 128x64 pixel display using I2C to communicate
+ 3 pins are required to interface (two I2C and one reset).
+
+ Adafruit invests time and resources providing this open
+ source code, please support Adafruit and open-source
+ hardware by purchasing products from Adafruit!
+
+ Written by Limor Fried/Ladyada for Adafruit Industries,
+ with contributions from the open source community.
+ BSD license, check license.txt for more information
+ All text above, and the splash screen below must be
+ included in any redistribution.
+ **************************************************************************/
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS D3
+
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+int deviceCount = 0;
+float temp0;
+float temp1;
+
+void displayTemperature(void) {
+  display.clearDisplay();
+
+  display.setTextSize(2);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(F("Temp"));             // Start at top-left corner
+  
+  display.setTextSize(2); 
+  sensors.requestTemperatures(); // Send the command to get temperatures
+
+
+  Serial.print("Temperature for the device 1 (index 0) is: [");
+  temp0 = sensors.getTempCByIndex(0);
+  temp1 = sensors.getTempCByIndex(1);
+
+  Serial.print(temp0);
+  Serial.print("] [");
+  Serial.print(temp1);
+  Serial.println("]");
+
+  display.println(temp0);
+  display.println(temp1);
+
+  display.display();
+}
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  sensors.begin();
+
+   // locate devices on the bus
+  Serial.print("Locating devices...");
+  Serial.print("Found ");
+  deviceCount = sensors.getDeviceCount();
+  Serial.print(deviceCount, DEC);
+  Serial.println(" devices.");
+  Serial.println("");
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  displayTemperature();
+  display.clearDisplay();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  displayTemperature();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
